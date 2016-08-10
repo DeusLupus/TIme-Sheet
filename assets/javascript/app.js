8d550186e1463eca3d1c0f04f3c2b970f3253a63
Yavi Padilla <yavipadilla@theengineersair.valenciacc.edu>
2016-08-09 20:10:23 -0400

    added table ID tags

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,9 +4,9 @@ authDomain: "alphasquadtimesheet.firebaseapp.com",
 databaseURL: "https://alphasquadtimesheet.firebaseio.com",
 storageBucket: "alphasquadtimesheet.appspot.com",
 };
-firebase.initializeApp(config);
+  firebase.initializeApp(config);
 
-var dataRef = firebase.database();
+var database = firebase.database();
 
 
 // Initial Values
@@ -34,36 +34,36 @@ $("#addButton").on("click", function() {
 		role: role,
 		date: startDate,
 		monthRate: monthRate,
-		dateAdded: firebase.database.ServerValue.TIMESTAMP
+		//dateAdded: firebase.database.ServerValue.TIMESTAMP
 	});
 	// Don't refresh the page!
 	return false;
 });
 
 //Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
-//dataRef.ref().on("child_added", function(childSnapshot) {
+dataRef.ref().on("child_added", function(childSnapshot) {
 	// Log everything that's coming out of snapshot
-	// console.log(childSnapshot.val().name);
-	// console.log(childSnapshot.val().role);
-	// console.log(childSnapshot.val().date);
-	// console.log(childSnapshot.val().monthRate);
+	 console.log(childSnapshot.val().name);
+	 console.log(childSnapshot.val().role);
+	 console.log(childSnapshot.val().date);
+	 console.log(childSnapshot.val().monthRate);
 	
 
 	// full list of items to the well
 
-// 		$('#full-member-list').append("<div class='well'><span id='name'> "+childSnapshot.val().name+" </span><span id='email'> "+childSnapshot.val().email+" </span><span id='age'> "+childSnapshot.val().age+" </span><span id='comment'> "+childSnapshot.val().comment+" </span></div>");
+ 		$('#full-member-list').append("<div class='well'><span id='name'> "+childSnapshot.val().name+" </span><span id='email'> "+childSnapshot.val().email+" </span><span id='age'> "+childSnapshot.val().age+" </span><span id='comment'> "+childSnapshot.val().comment+" </span></div>");
 
 
 // // Handle the errors
-// }, function(errorObject){
+ }, function(errorObject){
 // 	//console.log("Errors handled: " + errorObject.code)
-// });
+ });
 
-// dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
+ dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
 // 	// Change the HTML to reflect
-// 	$("#namedisplay").html(snapshot.val().name);
-// 	$("#emaildisplay").html(snapshot.val().email);
-// 	$("#agedisplay").html(snapshot.val().age);
-// 	$("#commentdisplay").html(snapshot.val().comment);
-// })
+ 	$("#emp").html(snapshot.val().name);
+ 	$("#roleTwo").html(snapshot.val().role);
+ 	$("#sd").html(snapshot.val().date);
+ 	$("#mr").html(snapshot.val().monthRate);
+ })
 
